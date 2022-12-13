07a4d2817ddaf28ccccf99271c07c1df51147b43
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-12-13 17:13:00 +0600

    text transform commit

--- src/Pages/Sections/Banner/Banner.js
+++ src/Pages/Sections/Banner/Banner.js
@@ -3,11 +3,16 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='flex justify-center min-h-screen items-center bg-black'>
-            <div className='relative flex justify-center items-center box'>
-                <div className='absolute top-0 left-0 w-3 h-full rounded-xl bg-white z-10 lightbar'></div>
+        <div className='min-h-screen p-10'>
+            <div className='relative flex justify-center items-center box  bg-black'>
+                <div className='absolute top-0 left-0 w-2 h-full rounded-xl bg-white z-10 lightbar'></div>
                 <div className='topLayer absolute top-0 left-0 w-full h-full bg-black'></div>
-                <h1 className='text-5xl tracking-widest text-shadow-main font-zendots'>RAHAT ASHIK</h1>
+                <h1 className='text-4xl tracking-widest text-shadow-main font-zendots'>RAHAT ASHIK</h1>
+            </div>
+            <div className='rotation'>
+                <div className='flex justify-end'><h1 className='text-6xl font-extrabold font-zendots fill-none texts'>This is a text</h1></div>
+                <div className='flex justify-end'><h1 className='text-6xl font-extrabold font-zendots fill-none texts'>Now second text</h1></div>
+                <div className='flex justify-end'><h1 className='text-6xl font-extrabold font-zendots fill-none texts'>Lastly third text</h1></div>
             </div>
         </div>
     );
